6e14218cb8a0978821b678d739a905c2ecc892be
WorkWithSoham <sohamtembe98@gmail.com>
2023-08-04 14:18:23 -0500

    Backend/ - API handling implemented. PostgresDB data insertion implemented.

---
+++ backend/postgres.dockerfile
@@ -0,0 +1,5 @@
+FROM postgres:16beta1-alpine3.18
+
+LABEL authors="sohamsantoshtembe"
+LABEL description="Postgres image for Amesite Inc Assessment."
+LABEL version="1.0"
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser")
+
 const app = express();
+app.use(cors("*"))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'soham',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'password',
+    port: '5432'
+})
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
+});
+
+app.post("/userdata", (req, res) => {
+    const userInfo = req.body
+    pool.query('INSERT INTO amesite.user_data (email, name, grade) VALUES ($1, $2, $3) RETURNING *', [userInfo.email, userInfo.name, userInfo.grade], (error, response) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(201).send(`User grades added with id: ${response.rows[0].id}`)
+    })
 });
